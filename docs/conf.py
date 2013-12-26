cb23845b3f2e327d7f985bdc2b26643fd4f99ac2
Brian Abelson <brianabelson@gmail.com>
2013-12-25 21:34:45 -0800

    whups

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pr'
+html_theme = 'kr'
 
 html_theme_path = ['_themes']
 # Theme options are theme-specific and customize the look and feel of a theme
