f7c5dbf506464bdaca28e2a5e85822e9ea6eef02
Brian Abelson <brianabelson@gmail.com>
2013-12-27 12:48:44 -0800

    fixed command line interface

--- particle/cli.py
+++ particle/cli.py
@@ -2,31 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import json, yaml, os, sys
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 from particle.web import api
 from particle.app import Particle
 
 
+# sub-command functions
+def particle_runner(args):
+  tasks = args.tasks.split(',')
+  particle = Particle(filepath=args.config)
+  particle.run(tasks = tasks)
+
+def api_runner(args):
+  api(port = args.port, debug = args.debug)
+
+# command line interface
 def cli():
-  if sys.argv[1] == 'run':
-    parser = OptionParser()
-    parser.add_option("-c", '--config', dest="config", default="particle.yml")
-    parser.add_option("-t", '--tasks', dest="tasks", default="twitter,facebook,rssfeeds,promopages")
-    (options, args) = parser.parse_args()
-    tasks = options.tasks.split(',')
-    particle = Particle(filepath=options.config)
-    particle.run(tasks = tasks)
+  parser = ArgumentParser(
+    prog = 'particle', 
+    usage = '%(prog)s [options]', 
+    description='Run particle from the command line.'
+  )
+  subparsers = parser.add_subparsers()
+  
+  # run particle
+  parser_run = subparsers.add_parser('run')
+  parser_run.add_argument(
+    "-c", '--config', 
+    dest="config", 
+    default="particle.yml", 
+    help="The path to your configuration file.\ndefault = particle.yml"
+  )
+  parser_run.add_argument(
+    "-t", '--tasks', 
+    dest="tasks", 
+    default="twitter,facebook,rssfeeds,promopages", 
+    help = 'A comma-separated list of tasks to run.\ndefault = twitter,facebook,rssfeeds,promopages'
+  )
+  parser_run.set_defaults(func=particle_runner)
 
-  elif sys.argv[1] == 'api':
-    parser = OptionParser()
-    parser.add_option("-p", '--port', dest="port", default = 3030)
-    parser.add_option("-d", '--debug', dest="debug", default = True)
-    (options, args) = parser.parse_args()
+  parser_api = subparsers.add_parser('api')
+  parser_api.add_argument(
+    "-p", '--port', 
+    dest = "port", 
+    default = 3030, 
+    help = 'The port on which to serve the API.\ndefault = 3030'
+  )
+  parser_api.add_argument(
+    "-d", '--debug', 
+    dest = "debug", 
+    action='store_true', 
+    help = 'Whether or not to run the API in debug mode.\ndefault = True'
+  )
+  parser_api.set_defaults(func=api_runner)
 
-    if str(options.debug).lower() == 'true':
-        debug = True
-    else:
-        debug = False
-
-    api(port = options.port, debug = debug)
+  # parse args and run function
+  args = parser.parse_args()
+  args.func(args)
