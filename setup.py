da750d5b8db82aadb811f817aff6650d8363625e
Brian Abelson <brianabelson@gmail.com>
2013-12-29 22:03:40 -0800

    now at version 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='particle',
-    version='0.0.6',
+    version='0.0.7',
     description="",
     long_description="",
     classifiers=[
